cc37b41aafc9e15be56af8f32af124e637b07879
Nike073i <nike073i@mail.ru>
2023-11-05 12:34:00 +0400

    fix: Разбиение файла по классам, сохранение в AddOrderItemCommand

--- src/Core/SolforbTest.Application/Orders/Commands/AddOrderItem/AddOrderItemCommand.cs
+++ src/Core/SolforbTest.Application/Orders/Commands/AddOrderItem/AddOrderItemCommand.cs
@@ -1,55 +1,8 @@
 using MediatR;
-using Microsoft.EntityFrameworkCore;
-using SolforbTest.Application.Common.Exceptions;
-using SolforbTest.Application.Interfaces;
 using SolforbTest.Application.Orders.Dto;
-using SolforbTest.Application.Orders.Helpers;
-using SolforbTest.Domain;
 
 namespace SolforbTest.Application.Orders.Commands.AddOrderItem
 {
     public record AddOrderItemCommand(int OrderItemId, CreateOrderItemDto OrderItemDto)
         : IRequest<Unit>;
-
-    public class AddOrderItemCommandHandler : IRequestHandler<AddOrderItemCommand, Unit>
-    {
-        private readonly ISolforbDbContext _dbContext;
-
-        public AddOrderItemCommandHandler(ISolforbDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
-        public async Task<Unit> Handle(
-            AddOrderItemCommand request,
-            CancellationToken cancellationToken
-        )
-        {
-            (int orderId, var orderItemDto) = request;
-            var order =
-                await _dbContext.Orders.FirstOrDefaultAsync(o => o.Id == orderId, cancellationToken)
-                ?? throw new NotFoundException("Order", orderId);
-
-            if (order.Number == orderItemDto.Name)
-                throw new InvalidOrderNumberException(
-                    $"Номер заказа - \"{order.Number}\" совпадает с названием элемента заказа"
-                );
-
-            await _dbContext.Orders
-                .Entry(order)
-                .Collection(o => o.OrderItems!)
-                .LoadAsync(cancellationToken);
-
-            var orderItemExists = order.OrderItems!.Any(item => item.Name == orderItemDto.Name);
-            if (orderItemExists)
-                throw new AlreadyExistException(
-                    $"Элемент заказа \"{orderItemDto.Name}\" уже существует в заказе ({orderId})"
-                );
-
-            order.OrderItems!.Add(
-                new OrderItem(orderItemDto.Name, orderItemDto.Quantity, orderItemDto.Unit)
-            );
-            return Unit.Value;
-        }
-    }
 }
---
+++ src/Core/SolforbTest.Application/Orders/Commands/AddOrderItem/AddOrderItemCommandHandler.cs
@@ -0,0 +1,52 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SolforbTest.Application.Common.Exceptions;
+using SolforbTest.Application.Interfaces;
+using SolforbTest.Domain;
+
+namespace SolforbTest.Application.Orders.Commands.AddOrderItem
+{
+    public class AddOrderItemCommandHandler : IRequestHandler<AddOrderItemCommand, Unit>
+    {
+        private readonly ISolforbDbContext _dbContext;
+
+        public AddOrderItemCommandHandler(ISolforbDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(
+            AddOrderItemCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            (int orderId, var orderItemDto) = request;
+            var order =
+                await _dbContext.Orders.FirstOrDefaultAsync(o => o.Id == orderId, cancellationToken)
+                ?? throw new NotFoundException("Order", orderId);
+
+            if (order.Number == orderItemDto.Name)
+                throw new InvalidOrderNumberException(
+                    $"Номер заказа - \"{order.Number}\" совпадает с названием элемента заказа"
+                );
+
+            await _dbContext.Orders
+                .Entry(order)
+                .Collection(o => o.OrderItems!)
+                .LoadAsync(cancellationToken);
+
+            var orderItemExists = order.OrderItems!.Any(item => item.Name == orderItemDto.Name);
+            if (orderItemExists)
+                throw new AlreadyExistException(
+                    $"Элемент заказа \"{orderItemDto.Name}\" уже существует в заказе ({orderId})"
+                );
+
+            order.OrderItems!.Add(
+                new OrderItem(orderItemDto.Name, orderItemDto.Quantity, orderItemDto.Unit)
+            );
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
