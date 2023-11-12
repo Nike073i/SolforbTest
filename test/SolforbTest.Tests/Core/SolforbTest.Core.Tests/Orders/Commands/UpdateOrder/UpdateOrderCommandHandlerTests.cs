b7af91b4e629e2c22d9bfc9b9f8cfe0d417320a7
Nike073i <nike073i@mail.ru>
2023-11-12 12:52:07 +0400

    add: Тесты на команду и валидатор UpdateOrder

---
+++ test/SolforbTest.Tests/Core/SolforbTest.Core.Tests/Orders/Commands/UpdateOrder/UpdateOrderCommandHandlerTests.cs
@@ -0,0 +1,165 @@
+﻿using SolforbTest.Application.Common.Exceptions;
+using SolforbTest.Application.Orders.Commands.UpdateOrder;
+using SolforbTest.Core.Tests.Common;
+
+namespace SolforbTest.Core.Tests.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task UpdateOrderCommandHandler_UpdateNumber_Success()
+        {
+            var handler = new UpdateOrderCommandHandler(Context);
+            var orderForUpdate = TestData.OrderTestList[0];
+            var orderId = orderForUpdate.Id;
+            var newNumber = Guid.NewGuid().ToString();
+
+            await handler.Handle(
+                new UpdateOrderCommand(
+                    orderId,
+                    newNumber,
+                    orderForUpdate.Date,
+                    orderForUpdate.ProviderId
+                ),
+                CancellationToken.None
+            );
+
+            var storedOrder = Context.Orders.FirstOrDefault(o => o.Id == orderId);
+            storedOrder.ShouldNotBeNull();
+            storedOrder.Number.ShouldBe(newNumber);
+            storedOrder.Date.ShouldBe(orderForUpdate.Date);
+            storedOrder.ProviderId.ShouldBe(orderForUpdate.ProviderId);
+        }
+
+        [Fact]
+        public async Task UpdateOrderCommandHandler_UpdateProviderId_Success()
+        {
+            var handler = new UpdateOrderCommandHandler(Context);
+            var orderForUpdate = TestData.OrderTestList[0];
+            var orderId = orderForUpdate.Id;
+            var newProviderId = 3;
+
+            await handler.Handle(
+                new UpdateOrderCommand(
+                    orderId,
+                    orderForUpdate.Number,
+                    orderForUpdate.Date,
+                    newProviderId
+                ),
+                CancellationToken.None
+            );
+
+            var storedOrder = Context.Orders.FirstOrDefault(o => o.Id == orderId);
+            storedOrder.ShouldNotBeNull();
+            storedOrder.Number.ShouldBe(orderForUpdate.Number);
+            storedOrder.Date.ShouldBe(orderForUpdate.Date);
+            storedOrder.ProviderId.ShouldBe(newProviderId);
+        }
+
+        [Fact]
+        public async Task UpdateOrderCommandHandler_UpdateDate_Success()
+        {
+            var handler = new UpdateOrderCommandHandler(Context);
+            var orderForUpdate = TestData.OrderTestList[0];
+            var orderId = orderForUpdate.Id;
+            var newDate = DateTime.UtcNow;
+
+            await handler.Handle(
+                new UpdateOrderCommand(
+                    orderId,
+                    orderForUpdate.Number,
+                    newDate,
+                    orderForUpdate.ProviderId
+                ),
+                CancellationToken.None
+            );
+
+            var storedOrder = Context.Orders.FirstOrDefault(o => o.Id == orderId);
+            storedOrder.ShouldNotBeNull();
+            storedOrder.Number.ShouldBe(orderForUpdate.Number);
+            storedOrder.Date.ShouldBe(newDate);
+            storedOrder.ProviderId.ShouldBe(orderForUpdate.ProviderId);
+        }
+
+        [Fact]
+        public async Task UpdateOrderCommandHandler_FailOnWrongOrderId()
+        {
+            var handler = new UpdateOrderCommandHandler(Context);
+            var orderIdForUpdate = 5000;
+            var newNumber = Guid.NewGuid().ToString();
+            var newProviderId = 3;
+            var newDate = DateTime.UtcNow;
+
+            await Assert.ThrowsAsync<NotFoundException>(
+                async () =>
+                    await handler.Handle(
+                        new UpdateOrderCommand(orderIdForUpdate, newNumber, newDate, newProviderId),
+                        CancellationToken.None
+                    )
+            );
+        }
+
+        [Fact]
+        public async Task UpdateOrderCommandHandler_ChangeNumber_FailByProviderAlreadyHaveOrder()
+        {
+            var handler = new UpdateOrderCommandHandler(Context);
+            var orderForUpdate = TestData.OrderTestList[0];
+            var newNumber = "Number 5";
+
+            await Assert.ThrowsAsync<AlreadyExistException>(
+                async () =>
+                    await handler.Handle(
+                        new UpdateOrderCommand(
+                            orderForUpdate.Id,
+                            newNumber,
+                            orderForUpdate.Date,
+                            orderForUpdate.ProviderId
+                        ),
+                        CancellationToken.None
+                    )
+            );
+        }
+
+        [Fact]
+        public async Task UpdateOrderCommandHandler_ChangeProvider_FailByProviderAlreadyHaveOrder()
+        {
+            var handler = new UpdateOrderCommandHandler(Context);
+            var orderForUpdate = TestData.OrderTestList[0];
+            var newProviderId = 2;
+
+            await Assert.ThrowsAsync<AlreadyExistException>(
+                async () =>
+                    await handler.Handle(
+                        new UpdateOrderCommand(
+                            orderForUpdate.Id,
+                            orderForUpdate.Number,
+                            orderForUpdate.Date,
+                            newProviderId
+                        ),
+                        CancellationToken.None
+                    )
+            );
+        }
+
+        [Fact]
+        public async Task UpdateOrderCommandHandler_ChangeNumber_FailByNumberEqualsOrderItemName()
+        {
+            var handler = new UpdateOrderCommandHandler(Context);
+            var orderForUpdate = TestData.OrderTestList[0];
+            var newNumber = "Name 1";
+
+            await Assert.ThrowsAsync<InvalidOrderNumberException>(
+                async () =>
+                    await handler.Handle(
+                        new UpdateOrderCommand(
+                            orderForUpdate.Id,
+                            newNumber,
+                            orderForUpdate.Date,
+                            orderForUpdate.ProviderId
+                        ),
+                        CancellationToken.None
+                    )
+            );
+        }
+    }
+}
---
+++ test/SolforbTest.Tests/Core/SolforbTest.Core.Tests/Orders/Commands/UpdateOrder/UpdateOrderCommandValidatorTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation.TestHelper;
+using SolforbTest.Application.Orders.Commands.UpdateOrder;
+
+namespace SolforbTest.Core.Tests.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandValidatorTests
+    {
+        private readonly UpdateOrderCommandValidator _validator;
+
+        public UpdateOrderCommandValidatorTests()
+        {
+            _validator = new UpdateOrderCommandValidator();
+        }
+
+        [Fact]
+        public void UpdateOrderCommandValidator_Success()
+        {
+            var command = CreateCommand();
+            var result = _validator.TestValidate(command);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void UpdateOrderCommandValidator_FailOnWrongOrderId()
+        {
+            var command = CreateCommand(orderId: 0);
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(c => c.OrderId);
+        }
+
+        [Fact]
+        public void UpdateOrderCommandValidator_FailOnEmptyNumber()
+        {
+            var command = CreateCommand(number: string.Empty);
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(c => c.Number);
+        }
+
+        [Fact]
+        public void UpdateOrderCommandValidator_FailOnWrongProviderId()
+        {
+            var command = CreateCommand(providerId: 0);
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(c => c.ProviderId);
+        }
+
+        [Theory]
+        [InlineData(1999, 1, 1)]
+        [InlineData(2101, 1, 1)]
+        public void UpdateOrderCommandValidator_FailOnWrongDate(int year, int month, int day)
+        {
+            var command = CreateCommand(date: new DateTime(year, month, day));
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(c => c.Date);
+        }
+
+        private UpdateOrderCommand CreateCommand(
+            int orderId = 1,
+            string number = "Number",
+            int providerId = 1,
+            DateTime? date = null
+        ) => new UpdateOrderCommand(orderId, number, date ?? DateTime.UtcNow, providerId);
+    }
+}
