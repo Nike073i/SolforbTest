98ede5b1152177e5d53f1a1669557d32858f2fa7
Nike073i <nike073i@mail.ru>
2023-11-12 14:17:12 +0400

    ref: Переименование UpdateOrderItemValidator

--- src/Core/SolforbTest.Application/OrderItems/Commands/UpdateOrderItem/UpdateOrderItemCommandValidator.cs
+++ src/Core/SolforbTest.Application/OrderItems/Commands/UpdateOrderItem/UpdateOrderItemCommandValidator.cs
@@ -3,9 +3,9 @@ using SolforbTest.Application.OrderItems.Validators;
 
 namespace SolforbTest.Application.OrderItems.Commands.UpdateOrderItem
 {
-    public class UpdateOrderItemValidator : AbstractValidator<UpdateOrderItemCommand>
+    public class UpdateOrderItemCommandValidator : AbstractValidator<UpdateOrderItemCommand>
     {
-        public UpdateOrderItemValidator(OrderItemDtoValidator orderItemDtoValidator)
+        public UpdateOrderItemCommandValidator(OrderItemDtoValidator orderItemDtoValidator)
         {
             RuleFor(command => command.OrderId).GreaterThan(0);
             RuleFor(command => command.OrderItemId).GreaterThan(0);
