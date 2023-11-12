29bcf5ecb44eeee31bf18be6b8f4f2936f0d1c93
Nike073i <nike073i@mail.ru>
2023-11-12 13:55:32 +0400

    add: Тесты на GetOrderItemUnits

---
+++ test/SolforbTest.Tests/Core/SolforbTest.Core.Tests/OrderItems/Queries/GetOrderItemUnitsQueryHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using SolforbTest.Application.OrderItems.Queries.GetOrderItemUnits;
+using SolforbTest.Core.Tests.Common;
+using SolforbTest.EfContext.Context;
+
+namespace SolforbTest.Core.Tests.OrderItems.Queries
+{
+    [Collection(nameof(QueryCollection))]
+    public class GetOrderItemUnitsQueryHandlerTests
+    {
+        private readonly SolforbDbContext _context;
+
+        public GetOrderItemUnitsQueryHandlerTests(QueryTestFixture fixture)
+        {
+            _context = fixture.Context;
+        }
+
+        [Fact]
+        public async Task GetOrderItemUnitsQueryHandlerTests_Success()
+        {
+            var handler = new GetOrderItemUnitsQueryHandler(_context);
+
+            var result = await handler.Handle(new GetOrderItemUnitsQuery(), CancellationToken.None);
+
+            var units = TestData.OrderTestList.SelectMany(
+                o => o.OrderItems?.Select(i => i.Unit) ?? Enumerable.Empty<string>()
+            );
+            var uniqueUnits = units.Distinct();
+            result.Count().ShouldBe(uniqueUnits.Count());
+        }
+    }
+}
