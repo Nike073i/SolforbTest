e07f9d4442a7cfbcfaf3137cfc569b423f17c427
Nike073i <nike073i@mail.ru>
2023-11-12 15:18:49 +0400

    add: Тесты команды и валидатора AddOrderItemCommand

---
+++ test/SolforbTest.Tests/Core/SolforbTest.Core.Tests/OrderItems/Commands/AddOrderItem/AddOrderItemCommandHandlerTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolforbTest.Application.Common.Exceptions;
+using SolforbTest.Application.OrderItems.Commands.AddOrderItem;
+using SolforbTest.Application.OrderItems.Dto;
+using SolforbTest.Core.Tests.Common;
+
+namespace SolforbTest.Core.Tests.OrderItems.Commands.AddOrderItem
+{
+    public class AddOrderItemCommandHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task AddOrderItemCommandHandler_Success()
+        {
+            var handler = new AddOrderItemCommandHandler(Context);
+            var orderId = TestData.TestOrderId;
+            var orderItemDto = CreateDto();
+            var orderItemId = await handler.Handle(
+                new AddOrderItemCommand(orderId, orderItemDto),
+                CancellationToken.None
+            );
+
+            var storedOrder = await Context.Orders.SingleOrDefaultAsync(o => o.Id == orderId);
+            storedOrder.ShouldNotBeNull();
+            var storedOrderItem = storedOrder.OrderItems!.FirstOrDefault(i => i.Id == orderItemId);
+            storedOrderItem.ShouldNotBeNull();
+            storedOrderItem.Name.ShouldBe(orderItemDto.Name);
+            storedOrderItem.Unit.ShouldBe(orderItemDto.Unit);
+            storedOrderItem.Quantity.ShouldBe(orderItemDto.Quantity);
+        }
+
+        [Fact]
+        public async Task AddOrderItemCommandHandler_FailOnWrongOrderId()
+        {
+            var handler = new AddOrderItemCommandHandler(Context);
+            var orderId = 500;
+            var orderItemDto = CreateDto();
+            await Assert.ThrowsAsync<NotFoundException>(
+                async () =>
+                    await handler.Handle(
+                        new AddOrderItemCommand(orderId, orderItemDto),
+                        CancellationToken.None
+                    )
+            );
+        }
+
+        [Fact]
+        public async Task AddOrderItemCommandHandler_FailByOrderAlreadyHaveName()
+        {
+            var handler = new AddOrderItemCommandHandler(Context);
+            var orderId = TestData.TestOrderId;
+            var orderItemDto = CreateDto(name: TestData.TestOrderItemName);
+            await Assert.ThrowsAsync<AlreadyExistException>(
+                async () =>
+                    await handler.Handle(
+                        new AddOrderItemCommand(orderId, orderItemDto),
+                        CancellationToken.None
+                    )
+            );
+        }
+
+        [Fact]
+        public async Task AddOrderItemCommandHandler_FailByOrderNumberEqualsName()
+        {
+            var handler = new AddOrderItemCommandHandler(Context);
+            var orderId = TestData.TestOrderId;
+            var orderItemDto = CreateDto(name: TestData.TestOrderNumber);
+            await Assert.ThrowsAsync<InvalidOrderNumberException>(
+                async () =>
+                    await handler.Handle(
+                        new AddOrderItemCommand(orderId, orderItemDto),
+                        CancellationToken.None
+                    )
+            );
+        }
+
+        private static OrderItemDto CreateDto(
+            string? name = null,
+            decimal quantity = 15,
+            string unit = "kg"
+        ) => new(name ?? Guid.NewGuid().ToString(), quantity, unit);
+    }
+}
---
+++ test/SolforbTest.Tests/Core/SolforbTest.Core.Tests/OrderItems/Commands/AddOrderItem/AddOrderItemCommandValidatorTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation.TestHelper;
+using SolforbTest.Application.OrderItems.Commands.AddOrderItem;
+using SolforbTest.Application.OrderItems.Dto;
+using SolforbTest.Application.OrderItems.Validators;
+
+namespace SolforbTest.Core.Tests.OrderItems.Commands.AddOrderItem
+{
+    public class AddOrderItemCommandValidatorTests
+    {
+        private readonly AddOrderItemCommandValidator _validator;
+
+        public AddOrderItemCommandValidatorTests()
+        {
+            var orderItemValidator = new OrderItemDtoValidator();
+            _validator = new AddOrderItemCommandValidator(orderItemValidator);
+        }
+
+        [Fact]
+        public void AddOrderItemCommandValidator_Success()
+        {
+            var command = CreateCommand();
+            var result = _validator.TestValidate(command);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void AddOrderItemCommandValidator_FailOnWrongOrderId()
+        {
+            var command = CreateCommand(orderId: 0);
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(c => c.OrderId);
+        }
+
+        private AddOrderItemCommand CreateCommand(int orderId = 1)
+        {
+            var orderItemDto = new OrderItemDto("Name", 15m, "Unit");
+            return new AddOrderItemCommand(orderId, orderItemDto);
+        }
+    }
+}
