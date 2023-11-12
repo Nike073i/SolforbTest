c57a44a6ae607bd444fcbe8a9bc9ebe68a8c583b
Nike073i <nike073i@mail.ru>
2023-11-12 12:00:45 +0400

    add: Тесты на команду и валидатор CreateOrder

---
+++ test/SolforbTest.Tests/Core/SolforbTest.Core.Tests/Orders/Commands/CreateOrder/CreateOrderCommandHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using SolforbTest.Application.Common.Exceptions;
+using SolforbTest.Application.Orders.Commands.CreateOrder;
+using SolforbTest.Core.Tests.Common;
+
+namespace SolforbTest.Core.Tests.Orders.Commands.CreateOrder
+{
+    public class CreateOrderCommandHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task CreateOrderCommandHandler_Success()
+        {
+            var handler = new CreateOrderCommandHandler(Context);
+            int providerId = 1;
+            string number = Guid.NewGuid().ToString();
+
+            var newOrderId = await handler.Handle(
+                new CreateOrderCommand(number, providerId),
+                CancellationToken.None
+            );
+
+            newOrderId.ShouldBeGreaterThan(0);
+            var storedOrder = Context.Orders.FirstOrDefault(o => o.Id == newOrderId);
+            storedOrder.ShouldNotBeNull();
+            storedOrder.Number.ShouldBe(number);
+            storedOrder.ProviderId.ShouldBe(providerId);
+            storedOrder.Date.ShouldBeInRange(new DateTime(), DateTime.UtcNow);
+        }
+
+        [Fact]
+        public async Task CreateOrderCommandHandler_ProviderAlreadyHaveOrder_ThrowAlreadyExists()
+        {
+            var handler = new CreateOrderCommandHandler(Context);
+            int providerId = TestData.OrderTestList[0].ProviderId;
+            string number = TestData.OrderTestList[0].Number;
+
+            await Assert.ThrowsAsync<AlreadyExistException>(
+                async () =>
+                    await handler.Handle(
+                        new CreateOrderCommand(number, providerId),
+                        CancellationToken.None
+                    )
+            );
+        }
+    }
+}
---
+++ test/SolforbTest.Tests/Core/SolforbTest.Core.Tests/Orders/Commands/CreateOrder/CreateOrderCommandValidatorTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation.TestHelper;
+using SolforbTest.Application.Orders.Commands.CreateOrder;
+
+namespace SolforbTest.Core.Tests.Orders.Commands.CreateOrder
+{
+    public class CreateOrderCommandValidatorTests
+    {
+        private readonly CreateOrderCommandValidator _validator;
+
+        public CreateOrderCommandValidatorTests()
+        {
+            _validator = new CreateOrderCommandValidator();
+        }
+
+        [Fact]
+        public void CreateOrderCommandValidator_Success()
+        {
+            string number = Guid.NewGuid().ToString();
+            int providerId = 1;
+
+            var command = new CreateOrderCommand(number, providerId);
+            var result = _validator.TestValidate(command);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void CreateOrderCommandValidator_FailOnEmptyNumber()
+        {
+            string number = string.Empty;
+            int providerId = 1;
+            var command = new CreateOrderCommand(number, providerId);
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(q => q.Number);
+        }
+
+        [Fact]
+        public void CreateOrderCommandValidator_FailOnWrongProviderId()
+        {
+            string number = Guid.NewGuid().ToString();
+            int providerId = 0;
+            var command = new CreateOrderCommand(number, providerId);
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(q => q.ProviderId);
+        }
+    }
+}
