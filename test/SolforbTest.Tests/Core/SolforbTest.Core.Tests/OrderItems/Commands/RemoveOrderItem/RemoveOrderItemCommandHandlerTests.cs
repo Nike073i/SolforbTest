bb32436268a0e8b4b3e4022a9ad1ea1157fc41ed
Nike073i <nike073i@mail.ru>
2023-11-12 15:18:56 +0400

    add: Тесты команды и валидатора RemoveOrderItemCommand

---
+++ test/SolforbTest.Tests/Core/SolforbTest.Core.Tests/OrderItems/Commands/RemoveOrderItem/RemoveOrderItemCommandHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolforbTest.Application.Common.Exceptions;
+using SolforbTest.Application.OrderItems.Commands.RemoveOrderItem;
+using SolforbTest.Core.Tests.Common;
+
+namespace SolforbTest.Core.Tests.OrderItems.Commands.RemoveOrderItem
+{
+    public class RemoveOrderItemCommandHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task RemoveOrderItemCommandHandler_Success()
+        {
+            var handler = new RemoveOrderItemCommandHandler(Context);
+            var orderIdForDelete = TestData.TestOrderId;
+            var orderItemIdForDelete = TestData.TestOrderItemId;
+
+            await handler.Handle(
+                new RemoveOrderItemCommand(orderIdForDelete, orderItemIdForDelete),
+                CancellationToken.None
+            );
+
+            var order = await Context.Orders.SingleOrDefaultAsync(o => o.Id == orderIdForDelete);
+            order.ShouldNotBeNull();
+            order.OrderItems!.ShouldNotContain(i => i.Id == orderItemIdForDelete);
+        }
+
+        [Fact]
+        public async Task RemoveOrderItemCommandHandler_FailOnWrongOrderId()
+        {
+            var handler = new RemoveOrderItemCommandHandler(Context);
+            var wrongOrderId = 5000;
+            var orderItemIdForDelete = TestData.TestOrderItemId;
+
+            await Assert.ThrowsAsync<NotFoundException>(
+                async () =>
+                    await handler.Handle(
+                        new RemoveOrderItemCommand(wrongOrderId, orderItemIdForDelete),
+                        CancellationToken.None
+                    )
+            );
+        }
+
+        [Fact]
+        public async Task RemoveOrderItemCommandHandler_FailOnWrongOrderItemId()
+        {
+            var handler = new RemoveOrderItemCommandHandler(Context);
+            var orderIdForDelete = TestData.TestOrderId;
+            var wrongOrderItemIdForDelete = 5000;
+
+            await Assert.ThrowsAsync<NotFoundException>(
+                async () =>
+                    await handler.Handle(
+                        new RemoveOrderItemCommand(orderIdForDelete, wrongOrderItemIdForDelete),
+                        CancellationToken.None
+                    )
+            );
+        }
+    }
+}
---
+++ test/SolforbTest.Tests/Core/SolforbTest.Core.Tests/OrderItems/Commands/RemoveOrderItem/RemoveOrderItemCommandValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation.TestHelper;
+using SolforbTest.Application.OrderItems.Commands.RemoveOrderItem;
+
+namespace SolforbTest.Core.Tests.OrderItems.Commands.RemoveOrderItem
+{
+    public class RemoveOrderItemCommandValidatorTests
+    {
+        private readonly RemoveOrderItemCommandValidator _validator;
+
+        public RemoveOrderItemCommandValidatorTests()
+        {
+            _validator = new RemoveOrderItemCommandValidator();
+        }
+
+        [Fact]
+        public void RemoveOrderItemCommandValidator_Success()
+        {
+            var command = CreateCommand();
+            var result = _validator.TestValidate(command);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void RemoveOrderItemCommandValidator_FailOnWrongOrderId()
+        {
+            var command = CreateCommand(orderId: 0);
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(c => c.OrderId);
+        }
+
+        [Fact]
+        public void RemoveOrderItemCommandValidator_FailOnWrongOrderItemId()
+        {
+            var command = CreateCommand(orderItemId: 0);
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(c => c.OrderItemId);
+        }
+
+        private RemoveOrderItemCommand CreateCommand(int orderId = 1, int orderItemId = 1001) =>
+            new RemoveOrderItemCommand(orderId, orderItemId);
+    }
+}
